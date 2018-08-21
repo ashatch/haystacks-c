fcaceae4db340e70f8f4ea515b230fbdf2781845
Andrew Hatch <ahatch@amplience.com>
2018-08-21 16:07:16 +0100

    conversion to array of 4x32-bit

--- uuid.c
+++ uuid.c
@@ -21,52 +21,45 @@ struct uuid* uuid_from_string(char *str) {
   u->value[2] = 0x0000;
   u->value[3] = 0x0000;
 
-  //ec3a49ae-927a-407b-a220-e86d1a4f084f
+  u->value[0] = u->value[0] |= hex2int(str[0])   << 28;
+  u->value[0] = u->value[0] |= hex2int(str[1])   << 24;
+  u->value[0] = u->value[0] |= hex2int(str[2])   << 20;
+  u->value[0] = u->value[0] |= hex2int(str[3])   << 16;
 
-  u->value[0] = u->value[0] |= hex2int(str[0])  << 12;
-  u->value[0] = u->value[0] |= hex2int(str[1])  << 8;
-  u->value[0] = u->value[0] |= hex2int(str[2])  << 4;
-  u->value[0] = u->value[0] |= hex2int(str[3])  << 0;
+  u->value[0] = u->value[0] |= hex2int(str[4])   << 12;
+  u->value[0] = u->value[0] |= hex2int(str[5])   << 8;
+  u->value[0] = u->value[0] |= hex2int(str[6])   << 4;
+  u->value[0] = u->value[0] |= hex2int(str[7])   << 0;
 
-//   u->lsb = 0x000000000000;
-//   u->msb = 0x000000000000;
+  u->value[1] = u->value[1] |= hex2int(str[9])   << 28;
+  u->value[1] = u->value[1] |= hex2int(str[10])  << 24;
+  u->value[1] = u->value[1] |= hex2int(str[11])  << 20;
+  u->value[1] = u->value[1] |= hex2int(str[12])  << 16;
 
-//   u->msb = u->msb |= hex2int(str[0])  << 60;
-//   u->msb = u->msb |= hex2int(str[1])  << 56;
-//   u->msb = u->msb |= hex2int(str[2])  << 52;
-//   u->msb = u->msb |= hex2int(str[3])  << 48;
-//   u->msb = u->msb |= hex2int(str[4])  << 44;
-//   u->msb = u->msb |= hex2int(str[5])  << 40;
-//   u->msb = u->msb |= hex2int(str[6])  << 36;
-//   u->msb = u->msb |= hex2int(str[7])  << 32;
+  u->value[1] = u->value[1] |= hex2int(str[14])  << 12;
+  u->value[1] = u->value[1] |= hex2int(str[15])  << 8;
+  u->value[1] = u->value[1] |= hex2int(str[16])  << 4;
+  u->value[1] = u->value[1] |= hex2int(str[17])  << 0;
 
-//   u->msb = u->msb |= hex2int(str[9])  << 28;
-//   u->msb = u->msb |= hex2int(str[10]) << 24;
-//   u->msb = u->msb |= hex2int(str[11]) << 20;
-//   u->msb = u->msb |= hex2int(str[12]) << 16;
+  u->value[2] = u->value[2] |= hex2int(str[19])  << 12;
+  u->value[2] = u->value[2] |= hex2int(str[20])  << 8;
+  u->value[2] = u->value[2] |= hex2int(str[21])  << 4;
+  u->value[2] = u->value[2] |= hex2int(str[22])  << 0;
 
-//   u->msb = u->msb |= hex2int(str[14]) << 12;
-//   u->msb = u->msb |= hex2int(str[15]) << 8;
-//   u->msb = u->msb |= hex2int(str[16]) << 4;
-//   u->msb = u->msb |= hex2int(str[17]) << 0;
+  u->value[2] = u->value[2] |= hex2int(str[24])  << 12;
+  u->value[2] = u->value[2] |= hex2int(str[25])  << 8;
+  u->value[2] = u->value[2] |= hex2int(str[26])  << 4;
+  u->value[2] = u->value[2] |= hex2int(str[27])  << 0;
 
-//   u->lsb = u->lsb |= hex2int(str[19]) << 60;
-//   u->lsb = u->lsb |= hex2int(str[20]) << 56;
-//   u->lsb = u->lsb |= hex2int(str[21]) << 52;
-//   u->lsb = u->lsb |= hex2int(str[22]) << 48;
+  u->value[3] = u->value[3] |= hex2int(str[28])  << 12;
+  u->value[3] = u->value[3] |= hex2int(str[29])  << 8;
+  u->value[3] = u->value[3] |= hex2int(str[30])  << 4;
+  u->value[3] = u->value[3] |= hex2int(str[31])  << 0;
 
-//   u->lsb = u->lsb |= hex2int(str[24]) << 44;
-//   u->lsb = u->lsb |= hex2int(str[25]) << 40;
-//   u->lsb = u->lsb |= hex2int(str[26]) << 36;
-//   u->lsb = u->lsb |= hex2int(str[27]) << 32;
-//   u->lsb = u->lsb |= hex2int(str[28]) << 28;
-//   u->lsb = u->lsb |= hex2int(str[29]) << 24;
-//   u->lsb = u->lsb |= hex2int(str[30]) << 20;
-//   u->lsb = u->lsb |= hex2int(str[31]) << 16;
-//   u->lsb = u->lsb |= hex2int(str[32]) << 12;
-//   u->lsb = u->lsb |= hex2int(str[33]) << 8;
-//   u->lsb = u->lsb |= hex2int(str[34]) << 4;
-//   u->lsb = u->lsb |= hex2int(str[35]) << 0;
-  
+  u->value[3] = u->value[3] |= hex2int(str[32])  << 12;
+  u->value[3] = u->value[3] |= hex2int(str[33])  << 8;
+  u->value[3] = u->value[3] |= hex2int(str[34])  << 4;
+  u->value[3] = u->value[3] |= hex2int(str[35])  << 0;
+
   return u;
 }
