336a82c6ead328fcfb53f9ecad6dfff43481435a
Andrew Hatch <ahatch@amplience.com>
2019-02-08 12:16:11 +0000

    use calloc for memory initialization

--- haystacks.c
+++ haystacks.c
@@ -8,7 +8,7 @@
 #define MAX_LINE_LENGTH 256
 
 void loadNeedles(FILE *needlesFile, Dict *dict) {
-  char *buf = malloc(sizeof(char) * MAX_LINE_LENGTH);
+  char *buf = calloc(sizeof(char), MAX_LINE_LENGTH);
 	while (fgets(buf, MAX_LINE_LENGTH, needlesFile)) {
     DictInsert(*dict, buf, "");
 	}
@@ -17,7 +17,7 @@ void loadNeedles(FILE *needlesFile, Dict *dict) {
 }
 
 void processStdin(Dict *dict) {
-	char *buf = malloc(sizeof(char) * MAX_LINE_LENGTH);
+	char *buf = calloc(sizeof(char), MAX_LINE_LENGTH);
 
 	while (fgets(buf, MAX_LINE_LENGTH, stdin) != NULL) {
       if (DictSearch(*dict, buf) != 0) {
